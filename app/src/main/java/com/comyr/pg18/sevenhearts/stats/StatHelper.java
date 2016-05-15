6f2e07b9603e1e08f756749d6470a31bc3f6b6c2
gaikwad_pranav <gaikwad.pranav@gmail.com>
2016-05-15 15:30:51 +0530

    added new drawables

--- app/src/main/java/com/comyr/pg18/sevenhearts/stats/StatHelper.java
+++ app/src/main/java/com/comyr/pg18/sevenhearts/stats/StatHelper.java
@@ -6,31 +6,41 @@ import com.comyr.pg18.sevenhearts.stats.listeners.OnAchievementUnlockedListener;
 import com.comyr.pg18.sevenhearts.utils.PreferenceHelper;
 
 /**
- * Created by pranav on 5/13/16.
- * On 12:49 PM
- * This class has been created to manipulate game <b>statistics.</b>
+ * @author Pranav
+ * <h1>Game statistics manipulation</h1>
+ * <p>This class has been created to manipulate game statistics.
  * Game statistics basically include numbers that keep track of below
- * mentioned properties :
- * 1. Total games played
- * 2. Total games won
- * 3. Games won in a row
- * 4. Games played in one instance of the application
- * 5. Total points scored (To be decided)
- * This class is used to maintain achievements and leaderboards in Google
- * play games console.
+ * mentioned properties.</p>
+ * <ul>
+ * <li>Total games played</li>
+ * <li>Total games won</li>
+ * <li>Games won in a row</li>
+ * <li>Games played in one instance of the application</li>
+ * <li>Total points scored (To be decided)</li>
+ * </ul>
+ * <p>This class is used to maintain achievements and leaderboards in Google
+ * play games console.</p>
  */
 public class StatHelper {
     /**
-     * Broadcasts achievements
+     * This class has various methods that perform operations on data
+     * related to player's statistics. For every such operation that
+     * takes place on player's statistics, {@link #l} will be used to call
+     * corresponding methods of {@link OnAchievementUnlockedListener}
+     * Whichever class that implements {@link OnAchievementUnlockedListener} will be
+     * able to receive statistics data.
+     * @see OnAchievementUnlockedListener#onGamesPlayed(int)
+     * @see OnAchievementUnlockedListener#onGamesWon(int)
+     * @see OnAchievementUnlockedListener#onGamesWonInRow(int)
      */
     public static OnAchievementUnlockedListener l;
 
     /**
-     * Increments the count of total games played by the user
+     * Increments the count of total games played by the user by one.
      *
-     * @param context Activity context
+     * @param context {@link Context} of activity that calls this method.
      */
-    private static void incrementGamesPlayed(Context context) {
+    public static void incrementGamesPlayed(Context context) {
         int o = PreferenceHelper.getInstance(context).ri(PreferenceHelper.KEY_GAMES_PLAYED);
         int n;
         if (o == PreferenceHelper.STATUS_INT_NOT_FOUND) {
@@ -44,11 +54,11 @@ public class StatHelper {
     }
 
     /**
-     * Increments games won by the player
+     * Increments games won by the player by one.
      *
-     * @param context Activity context
+     * @param context {@link Context} of activity that calls this method.
      */
-    private static void incrementGamesWon(Context context) {
+    public static void incrementGamesWon(Context context) {
         int o = PreferenceHelper.getInstance(context).ri(PreferenceHelper.KEY_GAMES_WON);
         int n;
         if (o == PreferenceHelper.STATUS_INT_NOT_FOUND) {
@@ -62,7 +72,9 @@ public class StatHelper {
     }
 
     /**
-     * Increments games played in a row by 1
+     * Increments games won in row by one.
+     *
+     * @param context {@link Context} of activity that calls this method.
      */
     public static void incrementGamesWonInRow(Context context) {
         int o = PreferenceHelper.getInstance(context).ri(PreferenceHelper.KEY_GAMES_WON_IN_ROW);
@@ -78,18 +90,18 @@ public class StatHelper {
     }
 
     /**
-     * Re-initializes <b>'games won in a row'</b> property to 0
+     * Re-initializes <b>'games won in a row'</b> to zero.
      *
-     * @param context Activity context
+     * @param context {@link Context} of activity that calls this method.
      */
     public static void resetGamesWonInRow(Context context) {
         PreferenceHelper.getInstance(context).w(PreferenceHelper.KEY_GAMES_WON_IN_ROW, 0);
     }
 
     /**
-     * Listener to broadcast statistics changes
+     * Sets #l to given value.
      *
-     * @param l {@link OnAchievementUnlockedListener} object
+     * @param l {@link OnAchievementUnlockedListener} object of the class that implements this interface.
      */
     public static void setOnAchievementUnlockedListener(OnAchievementUnlockedListener l) {
         StatHelper.l = l;
--- app/src/main/java/com/comyr/pg18/sevenhearts/stats/listeners/OnAchievementUnlockedListener.java
+++ app/src/main/java/com/comyr/pg18/sevenhearts/stats/listeners/OnAchievementUnlockedListener.java
@@ -1,8 +1,8 @@
 package com.comyr.pg18.sevenhearts.stats.listeners;
 
 /**
- * Created by pranav on 5/13/16.
- * On 1:20 PM
+ * @author Pranav
+ *
  */
 public interface OnAchievementUnlockedListener {
     void onGamesPlayed(int count);
